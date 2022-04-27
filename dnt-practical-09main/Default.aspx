8da20ccb1998fda77360743f54ef256eb3dc1087
yjpatel0 <61009655+yjpatel0@users.noreply.github.com>
2022-04-27 11:48:36 +0530

    Add files via upload

---
+++ dnt-practical-09main/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practical9.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <h3>Image Upload</h3>
+
+    <form id="Form1" runat="server">
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+    <br /><br />
+    <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="Upload" />
+   <br />
+    <hr />
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" ShowHeader="false">
+    <Columns>
+      
+        <asp:ImageField DataImageUrlField="Value" ControlStyle-Height="100" ControlStyle-Width="100" />
+          <asp:BoundField DataField="Text" /> 
+    </Columns>
+    </asp:GridView>
+    </form>   
+</body>
+</html>
---
+++ dnt-practical-09main/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical9
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            displayImages();
+        }
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                if (FileUpload1.FileName.EndsWith("png") || FileUpload1.FileName.EndsWith("jpg") || FileUpload1.FileName.EndsWith("jpeg"))
+                {
+                    string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Images/") + fileName);
+                    Response.Redirect(Request.Url.AbsoluteUri);
+                }
+            }
+        }
+
+        void displayImages()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/Images/"));
+            ListItemCollection files = new ListItemCollection();
+            foreach (string filePath in filePaths)
+            {
+                string fileName = Path.GetFileName(filePath);
+                files.Add(new ListItem(fileName, "~/Images/" + fileName));
+
+            }
+            GridView1.DataSource = files;
+            GridView1.DataBind();
+        }
+    }
+}
